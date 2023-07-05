06e095a3a080ac92d0a71dd7748ef1e06daaae3c
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-05 20:17:47 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'monica';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name) {
+    return bestCustomer = name
+}
+
+function changeLeastFavoriteCustomer(name) {
+    return leastFavoriteCustomer = name
+}
